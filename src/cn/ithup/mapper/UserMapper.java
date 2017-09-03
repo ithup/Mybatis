ceb7ba2b91c4418a055f36996fd892d14696e2fe
ithuplion <1805421482@qq.com>
2017-09-03 15:00:39 +0800

    mybatis高级应用

--- src/cn/ithup/mapper/UserMapper.java
+++ src/cn/ithup/mapper/UserMapper.java
@@ -2,28 +2,77 @@ package cn.ithup.mapper;
 
 import java.util.List;
 
+import cn.ithup.pojo.CustomOrders;
+import cn.ithup.pojo.Orders;
+import cn.ithup.pojo.QueryVo;
 import cn.ithup.pojo.User;
-
 
 public interface UserMapper {
 
 	/**
 	 * 根据id查询一条用户信息
+	 * 
 	 * @param id
 	 * @return
 	 */
 	public User findUserById(Integer id);
-	
+
 	/**
 	 * 根据用户名称模糊查询用户信息
+	 * 
 	 * @param userName
 	 * @return
 	 */
 	public List<User> findUserByUserName(String userName);
-	
+
 	/**
 	 * 添加一条用户信息
+	 * 
 	 * @param user
 	 */
 	public void insertUser(User user);
+
+	/**
+	 * 根据用户名查询用户信息，查询条件放到QueryVo的user属性中
+	 * 
+	 * @param queryVo
+	 * @return
+	 */
+	public List<User> findUserByUserQueryVo(QueryVo queryVo);
+
+	/**
+	 * 查询用户数量
+	 * 
+	 * @return
+	 */
+	public int findUserCount();
+
+	/**
+	 * 根据用户名称和性别查询用户信息
+	 * 
+	 * @param user
+	 * @return
+	 */
+	public List<User> findUserByUserNameAndSex(User user);
+
+	/**
+	 * 批量查询
+	 * 
+	 * @param queryVo
+	 * @return
+	 */
+	public List<User> findUserByIds(QueryVo queryVo);
+
+	/**
+	 * 批量删除
+	 * 
+	 * @param queryVo
+	 */
+	public void deleteUserByIds(QueryVo queryVo);
+
+	public List<CustomOrders> findOrdersAndUser();
+
+	public List<Orders> findOrdersAndUser2();
+
+	public List<User> findUserAndOrders();
 }
--- src/cn/ithup/test/UserMapperTest.java
+++ src/cn/ithup/test/UserMapperTest.java
@@ -1,6 +1,7 @@
 package cn.ithup.test;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -12,6 +13,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import cn.ithup.mapper.UserMapper;
+import cn.ithup.pojo.CustomOrders;
+import cn.ithup.pojo.Orders;
+import cn.ithup.pojo.QueryVo;
 import cn.ithup.pojo.User;
 
 
@@ -65,4 +69,101 @@ public class UserMapperTest {
 		
 		openSession.commit();
 	}
+	
+	@Test
+	public void testFindByQueryVo(){
+		SqlSession session = factory.openSession();
+		UserMapper mapper = session.getMapper(UserMapper.class);
+		QueryVo queryVo = new QueryVo();
+		User user = new User();
+		user.setUsername("小");
+		user.setSex("0");
+		queryVo.setUser(user);
+		List<User> list = mapper.findUserByUserQueryVo(queryVo);
+		for (User users : list) {
+			System.out.println(users);
+		}
+	}
+	
+	
+	@Test
+	public void testfindUserCount(){
+		SqlSession session = factory.openSession();
+		UserMapper mapper = session.getMapper(UserMapper.class);
+		int count = mapper.findUserCount();
+		System.out.println(count);
+	}
+	
+	@Test
+	public void testfindUserByUserNameAndSex(){
+		SqlSession session = factory.openSession();
+		UserMapper userMapper = session.getMapper(UserMapper.class);
+		User user = new User();
+		user.setUsername(null);
+		user.setSex(null);
+		List<User> list = userMapper.findUserByUserNameAndSex(user);
+		for (User user2 : list) {
+			System.out.println(user2);
+		}
+	}
+	
+	/**
+	 * 批量查询
+	 */
+	@Test
+	public void testfindUserByIds(){
+		SqlSession session = factory.openSession();
+		UserMapper userMapper = session.getMapper(UserMapper.class);
+		QueryVo queryVo = new QueryVo();
+		List<Integer> ids = new ArrayList<Integer>();
+		ids.add(11);
+		ids.add(16);
+		ids.add(22);
+		queryVo.setIds(ids );
+		List<User> list = userMapper.findUserByIds(queryVo);
+		for (User user : list) {
+			System.out.println(user);
+		}
+	}
+
+	/**
+	 * 批量删除
+	 */
+	@Test
+	public void testDeleteUserByIds(){
+		SqlSession session = factory.openSession();
+		UserMapper userMapper = session.getMapper(UserMapper.class);
+		QueryVo queryVo = new QueryVo();
+		List<Integer> ids = new ArrayList<Integer>();
+		ids.add(24);
+		ids.add(25);
+		queryVo.setIds(ids);
+		userMapper.deleteUserByIds(queryVo);
+	}
+	
+	@Test
+	public void testfindOrdersAndUser(){
+		SqlSession session = factory.openSession();
+		UserMapper userMapper = session.getMapper(UserMapper.class);
+		List<CustomOrders> list = userMapper.findOrdersAndUser();
+		for (CustomOrders customOrders : list) {
+			System.out.println(customOrders);
+		}
+	}
+	@Test
+	public void testfindOrdersAndUser2(){
+		SqlSession session = factory.openSession();
+		UserMapper userMapper = session.getMapper(UserMapper.class);
+		List<Orders> list = userMapper.findOrdersAndUser2();
+		for (Orders orders : list) {
+			System.out.println(orders);
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
 }
